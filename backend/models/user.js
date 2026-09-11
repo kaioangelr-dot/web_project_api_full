60c0c1ba6b9be7c761466302d641a4915eaf8649
KaioR <kaioangel.r@gmail.com>
2026-09-11 10:49:46 -0300

    feat: implement password and email in the mongoose schema

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,25 +3,36 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
   },
   avatar: {
     type: String,
-    required: true,
     validate: {
       validator: (v) => /^https?:\/\/(www\.)?[\w-]+(\.[\w-]+)*\.com(\/\S*)?$/.test(v), // prettier-ignore
       /* It starts with hhtp//: or https//:, does not allow consecutive dots, any space, and
       before the first .com it does not allow any other caracters than dots, digits,
       hifens and letters. It ends with either .com, .com/ or .com/'infinite caracters' */
     },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: 'Invalid email',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
   },
 });
 
