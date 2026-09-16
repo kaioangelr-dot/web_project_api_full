f8df786e0da51e5c548dfd75c19477f877c23b1c
KaioR <kaioangel.r@gmail.com>
2026-09-16 08:43:32 -0300

    fix: remove connection object from mongoose.connect

--- backend/app.js
+++ backend/app.js
@@ -47,10 +47,7 @@ app.use(express.json());
 app.use(cors());
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
   })
