4c248c52998f8f279910529802e725ec38d899e9
KaioR <kaioangel.r@gmail.com>
2026-09-16 19:55:43 -0300

    style: change spinner color for light mode

--- frontend/src/components/spinner/spinner.jsx
+++ frontend/src/components/spinner/spinner.jsx
@@ -3,12 +3,7 @@ import { ClipLoader } from "react-spinners";
 const Spinner = ({ isLoading }) => {
   return (
     <div className="spinner-container">
-      <ClipLoader
-        color="#fff"
-        align-self="center"
-        loading={isLoading}
-        size={50}
-      />
+      <ClipLoader className="spinner" loading={isLoading} />
     </div>
   );
 };
