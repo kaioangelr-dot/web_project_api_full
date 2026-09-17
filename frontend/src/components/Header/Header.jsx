505ca3a68ecf1f5702ed1edf858b1cd6d15b3dc8
KaioR <kaioangel.r@gmail.com>
2026-09-17 09:28:49 -0300

    fix: fix general styles switching mode erros

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -20,36 +20,32 @@ export default function Header({ isLoggedIn, setIsLoggedIn, email, isLight, setT
   return (
     <header className={`header page__section ${showoptions && "header__options"}`}>
       <img alt="Logotipo Around The U.S." className={`logo header__logo `} src={isLight ? logoBlack : logo} />
-      {isLoggedIn && (
-        <button
-          className={`header__menu-btn ${showoptions && "header__container_options-close-btn"}`}
-          onClick={handleShowoptions}
-        >
-          <span
-            className={`header__hamburger-bar ${!showoptions && "header__hamburger-bar_active"}`}
-            aria-hidden="true"
-          ></span>
-          <span
-            className={`header__hamburger-bar ${!showoptions && "header__hamburger-bar_active"}`}
-            aria-hidden="true"
-          ></span>
-          <span
-            className={`header__hamburger-bar ${!showoptions && "header__hamburger-bar_active"}`}
-            aria-hidden="true"
-          ></span>
-        </button>
-      )}
+      <button
+        className={`header__menu-btn ${showoptions && "header__container_options-close-btn"}`}
+        onClick={handleShowoptions}
+      >
+        <span
+          className={`header__hamburger-bar ${!showoptions && "header__hamburger-bar_active"}`}
+          aria-hidden="true"
+        ></span>
+        <span
+          className={`header__hamburger-bar ${!showoptions && "header__hamburger-bar_active"}`}
+          aria-hidden="true"
+        ></span>
+        <span
+          className={`header__hamburger-bar ${!showoptions && "header__hamburger-bar_active"}`}
+          aria-hidden="true"
+        ></span>
+      </button>
       <div className={`header__container ${showoptions && "header__container-options_active"}`}>
         <button className="header__switch-theme" onClick={() => setTheme(isLight)}></button>
-        {isLoggedIn ? (
+        {isLoggedIn && (
           <>
             <p className="header__title header__title-email">{`${email}`}</p>
             <button className="header__logout-btn" onClick={signout}>
               Sair
             </button>
           </>
-        ) : (
-          <p className="header__title">{location.pathname === "/signin" ? "Entrar" : "Cadastrar"}</p>
         )}
       </div>
     </header>
