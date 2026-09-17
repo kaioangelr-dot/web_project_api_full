4fb838e83be42630c9eeb3a78eff9fff5b2a576c
KaioR <kaioangel.r@gmail.com>
2026-09-17 11:29:43 -0300

    style: add a link for popup

--- frontend/src/components/InfoToolTip/components/IsAuthorized.jsx
+++ frontend/src/components/InfoToolTip/components/IsAuthorized.jsx
@@ -1,10 +1,14 @@
 import alright from "../../../images/alright.png";
+import { Link } from "react-router-dom";
 
 export default function IsAuthorized() {
   return (
     <>
       <img src={alright} alt="alright" className="popup__auth-image" />
-      <p className="popup__auth-text">Vitória! Você precisa fazer o login.</p>
+      <p className="popup__auth-text">Vitória! Você foi registrado.</p>
+      <Link to="/login" className="popup__login-link">
+        Fazer o login &#8594;
+      </Link>
     </>
   );
 }
